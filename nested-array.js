cae8de4a37e64063d3eee2b2c3fae5852853a505
darmariduan <dharmarhiduwhan@gmail.com>
2018-11-22 11:09:50 +0700

    Nested Array

---
+++ nested-array.js
@@ -0,0 +1,8 @@
+function panggilNestedArray() {
+  var dataNama = [["Ab","Ac","Ad"],
+                 ["Bola","Bola2","Bola3"],
+                 ["Rem1","Rem2","Rem3"]]
+console.log(dataNama.length);
+console.log(dataNama[1][2]);
+}
+panggilNestedArray()

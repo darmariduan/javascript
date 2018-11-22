3ac7362fd11a72f0026cf0b16511dfc0a50fbb83
darmariduan <dharmarhiduwhan@gmail.com>
2018-11-22 14:35:57 +0700

    Memanipulasi Array Dengan Shift

---
+++ array.shift.js
@@ -0,0 +1,11 @@
+function panggilShift() {
+  var kota = ['jakarta','bandung','surabaya','makasar'];
+  console.log(kota);
+  console.log("===============");
+  // kota = kota.shift();
+  var kota2 = kota.shift()
+  console.log(kota2);
+  return kota
+}
+console.log(panggilShift());
+// panggilShift()

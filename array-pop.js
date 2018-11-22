eaca612084e708d270b7cd4a1f5ec433295ac8c1
darmariduan <dharmarhiduwhan@gmail.com>
2018-11-22 14:43:11 +0700

    Memanipulasi Array Dengan POP

---
+++ array-pop.js
@@ -0,0 +1,7 @@
+function panggilPop() {
+  var kota = ['jakarta','bandung','medan','makasar'];
+  console.log(kota);
+  kota.pop()
+  return kota
+}
+console.log(panggilPop());
--- array.shift.js
+++ array.shift.js
@@ -3,6 +3,7 @@ function panggilShift() {
   console.log(kota);
   console.log("===============");
   // kota = kota.shift();
+  
   var kota2 = kota.shift()
   console.log(kota2);
   return kota

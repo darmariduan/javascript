81efe3b9986197e96d25ce7c5f68753ad0b1da9c
darmariduan <dharmarhiduwhan@gmail.com>
2018-11-22 13:40:52 +0700

    Array dalam Objects

--- objects.js
+++ objects.js
@@ -3,8 +3,10 @@ function panggilObjects() {
     type: 'Sedan',
     harga: 20000000000,
     warna: 'Putih',
+    tahun: [2001,2002,2003,2004]
   }
   console.log(mobil);
   console.log(mobil.harga);
+  console.log(mobil.tahun[3]);
 }
 panggilObjects()

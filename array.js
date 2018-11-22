bd4562dd78f6f93533b0c547b5fea470321462c3
darmariduan <dharmarhiduwhan@gmail.com>
2018-11-22 11:01:10 +0700

    Array

---
+++ array.js
@@ -0,0 +1,10 @@
+function PanggilArray() {
+  var data = [1,2,3,4,5,9]
+  var nama = ["abi","abi2","abi3"]
+
+  console.log(data.length);
+  console.log(nama.length);
+
+  console.log(nama[2]);
+}
+PanggilArray()

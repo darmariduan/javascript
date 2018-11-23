40661848827d8753ee44087c2c56aded7daedd26
darmariduan <dharmarhiduwhan@gmail.com>
2018-11-23 11:02:19 +0700

    Perulangan dengan foreach

---
+++ foreach.js
@@ -0,0 +1,10 @@
+function panggilforeach() {
+  var data = ['a','b','c'];
+  data.forEach(function (item, index, array){ //bebas
+  console.log(item);
+  console.log(index);
+  console.log(array);
+  })
+}
+
+panggilforeach()

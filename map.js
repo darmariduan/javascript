2dffd5e00895ba6390d8ffcb75839b5d17720c22
darmariduan <dharmarhiduwhan@gmail.com>
2018-11-23 11:30:15 +0700

    Perulangan dengan MAP

---
+++ map.js
@@ -0,0 +1,12 @@
+function panggilPerintahMAP() {
+  var kota = ['kisaran','deli serdang','tebing tinggi'];
+  kota.map(function(tes1, tes2,tes3){
+  // kota.map((tes1, tes2, tes3) => {
+  // alternatif penulisan diatas
+    console.log(tes1);
+    console.log(tes2);
+    console.log(tes3);
+  });
+}
+
+panggilPerintahMAP()

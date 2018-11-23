43b618fe10c7d427580b61d6504b530d0942449c
darmariduan <dharmarhiduwhan@gmail.com>
2018-11-23 10:06:10 +0700

    Mengenal perintah CONCAT

---
+++ array-concat.js
@@ -0,0 +1,8 @@
+function panggilConcat() {
+  var kota = ["jakarta","medan","bekasi","malang"];
+  var kabupaten = ["lampung timur","jambang","kediri"];
+
+  var hasil = kota.concat(kabupaten)
+  return hasil
+}
+console.log(panggilConcat());

f18f70697a73e69092a7fd68efd0b8d60c6cb485
darmariduan <dharmarhiduwhan@gmail.com>
2018-11-23 08:22:23 +0700

    Memanipulasi Array Dengan unshift

---
+++ array-unshift.js
@@ -0,0 +1,8 @@
+function panggilUnshift() {
+  var data = ["bandung","jakarta","medan","malang"];
+  console.log(data);
+  data.unshift("surabaya","bali");
+
+  return data
+}
+console.log(panggilUnshift());

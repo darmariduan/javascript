40fe3b28bede0dfaf0c61eb5cb53ea83ef3750c3
darmariduan <dharmarhiduwhan@gmail.com>
2018-11-23 10:27:21 +0700

    Perintah Reverse

---
+++ array-reverse.js
@@ -0,0 +1,9 @@
+function panggilReverse() {
+  var kota = ["jakarta","medan","malang","bandung"];
+  console.log(kota)
+
+  return kota.reverse()
+// var result = kota.reverse();
+// return result
+}
+console.log(panggilReverse());
